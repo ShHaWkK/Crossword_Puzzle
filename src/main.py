4758de62dbbafb74fbb54375ac3b1693ae3b1886
ShHaWkK <51519814+ShHaWkK@users.noreply.github.com>
2024-02-26 15:30:56 +0100

    ADD main.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from crossword import CrosswordPuzzle
+
+def main():
+    crossword = CrosswordPuzzle()
+    crossword.create_puzzle()
+    crossword.play()
+
+if __name__ == "__main__":
+    main()
